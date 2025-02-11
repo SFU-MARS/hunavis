0fccfab76276c5d9a1ac423c6b170d07027b58d0
mochen72 <mochen72@gmail.com>
2025-02-10 21:08:00 -0800

    add custom tb3 launch file that simulates obstacle detection from intel realsense's depth camera, and custom rviz

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         ("share/" + package_name, ["package.xml"]),
         (os.path.join("share", package_name, "launch"), glob("launch/*launch.py")),
         (os.path.join("share", package_name, "params"), glob("params/*")),
+        (os.path.join("share", package_name, "rviz"), glob("rviz/*")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
