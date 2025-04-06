9da62ae0729b4c7f99779cea102a7c35722c673e
mochen72 <mochen@cs.sfu.ca>
2025-04-06 02:48:56 +0000

    Update human detection and fixed bug in costmap params; update rviz

--- launch/hudet.launch.py
+++ launch/hudet.launch.py
@@ -1,30 +1,26 @@
-import json
 import math
 import os
 
-import yaml
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 
 from hunavis.utils import goal_from_params
 from launch import LaunchDescription
-from launch.actions import (DeclareLaunchArgument, IncludeLaunchDescription,
-                            OpaqueFunction)
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import (AndSubstitution, LaunchConfiguration,
-                                  NotSubstitution)
+from launch.substitutions import LaunchConfiguration, NotSubstitution
 
-"""
-Human Detection Launch File
-"""
+SCENARIO_PARAMS_FILE = "hunavsim.yaml"
 
-# Replace with yaml file containing parameters for hunavsim environment
 DEFAULT_PARAMS_FILE = os.path.join(
-    get_package_share_directory("hunavis"), "params", "params.yaml"
-)
-DEFAULT_RVIZ_FILE = os.path.join(
-    get_package_share_directory("hunavis"), "rviz", "default_sim_view.rviz"    
+    get_package_share_directory("hunavis"),
+    "params",
+    SCENARIO_PARAMS_FILE,
 )
 
 
@@ -39,9 +35,17 @@ def launch_setup(context, *args, **kwargs):
         condition=IfCondition(run_rviz),
         package="rviz2",
         executable="rviz2",
-        arguments=['-d', DEFAULT_RVIZ_FILE],
+        arguments=[
+            "-d"
+            + os.path.join(
+                get_package_share_directory("hunavis"),
+                "rviz",
+                "default_sim_view.rviz",
+            )
+        ],
         output={"both": "log"},
     )
+
     # Activate the following Nodes only if we are running experiments in the real world
     zed_wrapper_launch = IncludeLaunchDescription(
         # if not `use_simulator`
@@ -79,31 +83,10 @@ def launch_setup(context, *args, **kwargs):
             "--child-frame-id",
             "zed_camera_link",
         ],
-    )  # Values are specified manually, and would need to change if the camera pose changes wrt map
+    )  # Dummy values; change according to pose changes wrt map
 
     # Load list of human goals from the simulation parameters
     humans_goals_str = goal_from_params(params_file_val)
-
-    # Load list of human goals from the simulation parameters
-    with open(params_file_val, "r") as f:
-        config = yaml.safe_load(f)
-        params = config["hunav_loader"]["ros__parameters"]
-        humans_initial_pose = []
-        humans_goals = []
-        for agent in params["agents"]:
-            agent_goals = []
-            for goal_key in params[agent]["goals"]:
-                goal = params[agent][goal_key]
-                agent_goals.append([goal["x"], goal["y"]])
-
-            initial_pose = params[agent]["init_pose"]
-            initial_pose = [initial_pose["x"], initial_pose["y"]]
-
-            humans_initial_pose.append(initial_pose)
-            humans_goals.append(agent_goals)
-
-    # String representing list of goals as 2D np arrays
-    humans_goals_str = json.dumps(humans_goals)
 
     people_visualizer_node = Node(
         package="hunavis",
@@ -115,7 +98,7 @@ def launch_setup(context, *args, **kwargs):
     )
     return [
         rviz_node,
-        zed_wrapper_launch,
+        # zed_wrapper_launch,
         camera_map_tf,
         people_visualizer_node,
     ]
--- launch/tb3_custom_sim.launch.py
+++ launch/tb3_custom_sim.launch.py
@@ -179,7 +179,7 @@ def generate_launch_description():
     )
 
     declare_use_rviz_cmd = DeclareLaunchArgument(
-        "use_rviz", default_value="True", description="Whether to start RVIZ"
+        "use_rviz", default_value="False", description="Whether to start RVIZ"
     )
 
     declare_simulator_cmd = DeclareLaunchArgument(
